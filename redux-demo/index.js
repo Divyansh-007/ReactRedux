c09a1d6cac867e19939a4516c62ba7fe8fcdfcdd
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-06-07 10:33:40 +0530

    Learn redux middlewares

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,7 +1,10 @@
 const redux = require("redux");
+const reduxLogger = require("redux-logger");
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
-
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
 
@@ -80,12 +83,13 @@ const rootReducer = combineReducers({
   icecream: icecreamReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 console.log("Initial State", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log("Updated State", store.getState())
-);
+// const unsubscribe = store.subscribe(() =>
+//   console.log("Updated State", store.getState())
+// );
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyIceCream());
